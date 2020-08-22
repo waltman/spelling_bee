63b6d187ac779c8b7b92f90b340f801f9baf9395
Walt Mankowski <waltman@pobox.com>
2020-08-22 09:07:41 -0400

    added python 3 script to solve the puzzle
    This is almost 3x slower than the Perl code.

---
+++ spelling_bee.py
@@ -0,0 +1,17 @@
+from sys import argv
+import re
+
+def make_regex(c):
+    return re.compile(f'[^{c}]')
+
+DICT = '/usr/share/dict/words';
+c = argv[1]
+middle = c[0]
+regex = make_regex(c)
+
+with open(DICT) as f:
+    for line in f:
+        line = line.rstrip()
+        if len(line) < 4 or middle not in line or re.search(regex, line):
+            continue
+        print(line)
