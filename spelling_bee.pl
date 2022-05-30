5e9427654f824e13f0c73f628e9f732d1b9585a7
Walt Mankowski <waltman@pobox.com>
2022-05-30 19:54:11 -0400

    updated to v5.36

--- spelling_bee.pl
+++ spelling_bee.pl
@@ -1,8 +1,5 @@
 #!/usr/bin/env perl
-use strict;
-use warnings;
-use feature qw(:5.32);
-use experimental qw(signatures);
+use v5.36;
 use autodie;
 
 my $dict = '/usr/share/dict/american-english-huge';
